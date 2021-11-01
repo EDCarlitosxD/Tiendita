94f8edad9113f0fb3908f3d9bdb8f48dbc99bf27
EDCarlitosxD <juanuchdzib@gmail.com>
2021-11-01 11:31:20 -0500

    Termine interfaz y Codigo IfElse

---
+++ src/tiendas2/TiendaS2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiendas2;
+
+/**
+ *
+ * @author JUAN
+ */
+public class TiendaS2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Form a = new Form();
+        a.setVisible(true);
+        a.setLocationRelativeTo(a);
+        
+        
+    }
+    
+}
